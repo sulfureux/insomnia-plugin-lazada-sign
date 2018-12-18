c334e747c07d7e481ee76734cbf43aa75687b620
pierreneter <pierreneter@gmail.com>
2018-12-19 00:56:14 +0700

    dont require environment out of Lazada request

--- src/sign-query.js
+++ src/sign-query.js
@@ -5,21 +5,21 @@ const { parse } = require('url');
 
 module.exports = function (context) {
   const { request } = context;
-  const secret = request.getEnvironmentVariable('lazada_app_secret');
-  const appKey = request.getEnvironmentVariable('lazada_app_key');
-  const accessToken = request.getEnvironmentVariable('access_token');
-  if (!secret) {
-    window.alert(`Please set your 'lazada_app_secret' value at your environments`);
-    return;
-  }
-  if (!appKey) {
-    window.alert(`Please set your 'lazada_app_key' value at your environments`);
-    return;
-  }
   const toUrl = request.getUrl();
   const isApi = /api\.lazada/.test(toUrl);
   const isAuth = /auth\.lazada\.com\/rest/.test(toUrl);
   if (isApi || isAuth) {
+    const secret = request.getEnvironmentVariable('lazada_app_secret');
+    const appKey = request.getEnvironmentVariable('lazada_app_key');
+    const accessToken = request.getEnvironmentVariable('access_token');
+    if (!secret) {
+      window.alert(`Please set your 'lazada_app_secret' value at your environments`);
+      return;
+    }
+    if (!appKey) {
+      window.alert(`Please set your 'lazada_app_key' value at your environments`);
+      return;
+    }
     request.setParameter('timestamp', Date.now());
     request.setParameter('app_key', appKey);
     request.setParameter('sign_method', 'sha256');
